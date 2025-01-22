777381c3969496a876432c53ef9b3463c3c438ae
sergio77772 <sergio_77772@hotmail.com>
2025-01-22 17:09:30 -0300

    Update users.php

--- users.php
+++ users.php
@@ -1,7 +1,9 @@
 <?php
-header("Content-Type: application/json");
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 require 'db.php';
-
 $method = $_SERVER['REQUEST_METHOD'];
 $data = json_decode(file_get_contents("php://input"), true);
 
