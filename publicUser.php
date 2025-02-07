ffdf1454d9b603644a6b63120352914dd2a53518
sergio77772 <sergio_77772@hotmail.com>
2025-02-07 09:57:40 -0300

    Update publicUser.php

--- publicUser.php
+++ publicUser.php
@@ -1,7 +1,8 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *'); // Permite solicitudes desde cualquier dominio
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
